b6f185cf6d17bd8f00933914c044d7ebfa97d804
Anwesh43 <anweshthecool0@gmail.com>
2018-12-16 12:50:08 +0530

    adding LTPNode class

--- linetopipestepview/src/main/java/coml/anwesh/uiprojects/linetopipestepview/LineToPipeStepView.kt
+++ linetopipestepview/src/main/java/coml/anwesh/uiprojects/linetopipestepview/LineToPipeStepView.kt
@@ -119,4 +119,49 @@ class LineToPipeStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTPNode(var i : Int, val state : State = State()) {
+
+        private var next : LTPNode? = null
+
+        private var prev : LTPNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LTPNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTPNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTPNode {
+            var curr : LTPNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
