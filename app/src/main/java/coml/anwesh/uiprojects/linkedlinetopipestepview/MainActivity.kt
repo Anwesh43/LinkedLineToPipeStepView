fefe206b79717f04605461ec220394dcaca7d1af
Anwesh43 <anweshthecool0@gmail.com>
2018-12-16 13:06:05 +0530

    adding fullscreen support

--- app/src/main/java/coml/anwesh/uiprojects/linkedlinetopipestepview/MainActivity.kt
+++ app/src/main/java/coml/anwesh/uiprojects/linkedlinetopipestepview/MainActivity.kt
@@ -2,6 +2,7 @@ package coml.anwesh.uiprojects.linkedlinetopipestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import coml.anwesh.uiprojects.linetopipestepview.LineToPipeStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LineToPipeStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
