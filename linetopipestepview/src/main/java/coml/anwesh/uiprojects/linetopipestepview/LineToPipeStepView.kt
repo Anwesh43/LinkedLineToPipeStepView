e7523d18b1702899a9cf5d8e6d395eefa3161ac0
Anwesh43 <anweshthecool0@gmail.com>
2018-12-16 12:16:51 +0530

    adding libraries and defining constants

--- linetopipestepview/src/main/java/coml/anwesh/uiprojects/linetopipestepview/LineToPipeStepView.kt
+++ linetopipestepview/src/main/java/coml/anwesh/uiprojects/linetopipestepview/LineToPipeStepView.kt
@@ -4,3 +4,18 @@ package coml.anwesh.uiprojects.linetopipestepview
  * Created by anweshmishra on 16/12/18.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.Paint
+
+val nodes : Int = 5
+val lines : Int = 4
+val color : Int = Color.parseColor("#0D47A1")
+val sizeFactor : Float = 2.5f
+val strokeFactor : Int = 90
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
